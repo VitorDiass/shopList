d6e75db104911ef94019311c5f2ad1790f1c59e6
VitorDiass <vitordias28@hotmail.com>
2018-09-01 17:08:01 +0100

    Delete config.ts

--- src/app/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyDVs68yD4ZiAkvOHFwNT7T2liJfdnrtKjE",
-    authDomain: "shoplist-3ab56.firebaseapp.com",
-    databaseURL: "https://shoplist-3ab56.firebaseio.com",
-    projectId: "shoplist-3ab56",
-    storageBucket: "shoplist-3ab56.appspot.com",
-    messagingSenderId: "204844748266"
-  };
